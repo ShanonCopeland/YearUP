1ace85f8115881a7355df023fabc9d828eded4e1
ShanonCopeland <SCopeland@atl.yearup.org>
2015-04-24 22:45:58 -0400

    hello

---
+++ Project1/Intro/src/Intro.java
@@ -0,0 +1,15 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.print("Hello! Im Shanon Copeland. Welcome to the Java class");
+	
+	}
+
+}
+
+/* Syntax error, insert ")" to complete Expression
+ * Syntax error on token "]", after ] expected after this token
+ * Syntax error, insert "}" to complete CLassBpdy
+ * Syntax error on token "}", delete this token
+ */
